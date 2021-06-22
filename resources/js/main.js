eeaa515afef6e4e2f5aed2860dada1195b54069c
Shirshen Dada <72239897+shu-vro@users.noreply.github.com>
2021-06-23 00:37:16 +0600

    Add files via upload

---
+++ resources/js/main.js
@@ -0,0 +1,36 @@
+let boxes = document.querySelectorAll(".box");
+
+boxes.forEach((box) => {
+    box.addEventListener("mousemove", (e) => {
+        let halfWidth = box.clientWidth / 2,
+            halfHeight = box.clientHeight / 2,
+            mouseX = halfWidth + box.offsetLeft - e.pageX,
+            mouseY = halfHeight + box.offsetTop - e.pageY,
+            maxDeg = 10,
+            image = box.querySelector("img");
+
+        let degX = (mouseY / halfHeight) * -maxDeg + "deg";
+        let degY = (mouseX / halfWidth) * maxDeg + "deg";
+        console.log(degX, degY);
+        box.setAttribute(
+            "style",
+            `transform: perspective(512px) rotateX(${degX}) rotateY(${degY});`
+        );
+
+        image.setAttribute(
+            "style",
+            `object-position: ${
+                (box.offsetLeft + box.clientWidth - e.pageX) / -8 + 20
+            }px ${(box.offsetTop + box.clientHeight - e.pageY) / -8 + 20}px`
+        );
+    });
+
+    box.addEventListener("mouseout", () => {
+        box.removeAttribute("style");
+        box.querySelector("img").removeAttribute("style");
+    });
+});
+
+addEventListener("mousemove", (e) => {
+    // console.log(e);
+});
